b628af616e22745be5cfa7ce51e631c18f4b9c25
indrajith76 <indrajithgoswami@gmail.com>
2022-12-10 04:23:27 +0600

    update home component

--- src/pages/Home/FlowerBouquets/FlowerBouquets.js
+++ src/pages/Home/FlowerBouquets/FlowerBouquets.js
@@ -1,13 +1,14 @@
 import { Box, Button, Grid, styled, Typography } from "@mui/material";
 import React from "react";
-import PersonalizedGiftCard from "./PersonalizedGiftCard/PersonalizedGiftCard.js";
+import FlowerBouquetsGiftCard from "./FlowerBouquetsCard/FlowerBouquetsGiftCard.js";
 import personalizedGifts1 from "../../../assets/personalizedGifts/personalizedGifts1.png";
 import personalizedGifts2 from "../../../assets/personalizedGifts/personalizedGifts2.png";
 import personalizedGifts3 from "../../../assets/personalizedGifts/personalizedGifts3.png";
 import personalizedGifts4 from "../../../assets/personalizedGifts/personalizedGifts4.png";
 
-const PersonalizedGiftsSection = styled(Box)(({ theme }) => ({
+const FlowerBouquetsSection = styled(Box)(({ theme }) => ({
   textAlign: "center",
+  marginTop:'100px'
 }));
 
 const gifts = [
@@ -17,28 +18,28 @@ const gifts = [
   { img: personalizedGifts4 },
 ];
 
-const PersonalizedGifts = () => {
+const FlowerBouquets = () => {
   return (
-    <PersonalizedGiftsSection>
+    <FlowerBouquetsSection>
       <Typography
         color="textSecondary"
         sx={{ fontSize: "38px", fontWeight: "600" }}
       >
-        Popular Personalized Gifts
+        Fresh Flower Bouquets
       </Typography>
 
       <Grid container spacing={3} sx={{ marginTop: "50px" }}>
         {gifts.map((gift, index) => (
           <Grid item xs={12} sm={6} md={3} key={index}>
-            <PersonalizedGiftCard
+            <FlowerBouquetsGiftCard
               key={index}
               image={gift.img}
-            ></PersonalizedGiftCard>
+            ></FlowerBouquetsGiftCard>
           </Grid>
         ))}
       </Grid>
-    </PersonalizedGiftsSection>
+    </FlowerBouquetsSection>
   );
 };
 
-export default PersonalizedGifts;
+export default FlowerBouquets;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import Banner from "./Banner/Banner";
 import CustomDesignCake from "./CustomDesignCake/CustomDesignCake";
 import CustomGifts from "./CustomGifts/CustomGifts";
+import FeaturedProducts from "./FeaturedProducts/FeaturedProducts";
+import FlowerBouquetsGifts from "./FlowerBouquets/FlowerBouquets";
 import GetExclusiveOffer from "./GetExclusiveOffer/GetExclusiveOffer";
 import GiftCategory from "./GiftCategory/GiftCategory";
 import PersonalizedGifts from "./PersonalizedGifts/PersonalizedGifts";
@@ -15,6 +17,8 @@ const Home = () => {
       <GiftCategory></GiftCategory>
       <PersonalizedGifts></PersonalizedGifts>
       <CustomDesignCake></CustomDesignCake>
+      <FlowerBouquetsGifts></FlowerBouquetsGifts>
+      <FeaturedProducts></FeaturedProducts>
       <Testimonial></Testimonial>
       <GetExclusiveOffer></GetExclusiveOffer>
     </div>
